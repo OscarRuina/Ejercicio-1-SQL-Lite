b080e852e10177c25d9257bd9e8ccf691f2aa491
Oscar Ruina <31217980+OscarRuina@users.noreply.github.com>
2021-11-29 16:32:09 -0300

    Update Ejercicio.3.sql

--- Ejercicio.3.sql
+++ Ejercicio.3.sql
@@ -1,2 +1,2 @@
-insert into curso values ( 101, 'Algoritmos', 'Algoritmos y Estructuras de Datos', 'Mañana',35 )
+insert into curso values ( 101, 'Algoritmos', 'Algoritmos y Estructuras de Datos', 'Mañana',35 );
 insert into curso values ( 102, 'Matematica Discreta', '', 'Tarde',30 )
